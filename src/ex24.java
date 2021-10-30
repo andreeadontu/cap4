3529254f7a798451a7347f9316afcd2eefc942ea
andreeadontu <andreeadontu00@gmail.com>
2021-10-31 00:31:52 +0400

    Initial commit

---
+++ src/ex24.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ex24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int result;
+        int passes = 0;
+        int failures = 0;
+        int studentCounter = 1;
+        int x = 0;
+
+        while (studentCounter <= 10){
+            System.out.print("Enter result (1 = pass, 2 = fail): ");
+            result = input.nextInt();
+            if (result == 1){
+                passes++;
+                studentCounter++;
+            }
+            else if (result == 2){
+                failures++;
+                studentCounter++;
+            }
+            else
+                System.out.println("Insert another number (only 1 or 2)");
+        }
+
+        System.out.printf("Passed: %d%nFailed: %d%n", passes, failures);
+
+        if(passes > 8)
+            System.out.println("Bonus to instructor!");
+    }
+}
---
+++ src/ex32.java
@@ -0,0 +1,7 @@
+public class ex32 {
+    public static void main(String[] args){
+        for(int i=0; i<8; ++i){
+            System.out.printf("%s* * * * * * * *\n", (i % 2 != 0 ? " " : ""));
+        }
+    }
+}
